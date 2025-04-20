55bad2f311f962232b9b1f86119f496b7400a901
OscarAndres2823 <oscarandresortiz17@gmail.com>
2025-04-20 16:17:18 -0500

    portafolio

---
+++ web.js
@@ -0,0 +1,18 @@
+function abrirModal(id) {
+    document.getElementById(id).style.display = "flex";
+  }
+  
+  function cerrarModal(id) {
+    document.getElementById(id).style.display = "none";
+  }
+  
+  // Cierra el modal al hacer clic fuera del contenido
+  window.onclick = function (e) {
+    const modales = document.querySelectorAll(".modal");
+    modales.forEach(modal => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  }
+  
